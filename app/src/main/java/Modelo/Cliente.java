431b5a0376e674888616ad71506b66c4b272cfb5
Osvaldo <osvaldoadelatorretec@gmail.com>
2021-12-13 10:12:17 -0600

    Se modifico la clase de cliente, se le agrego lo nesesario para ser una entidad de room

--- app/src/main/java/Modelo/Cliente.java
+++ app/src/main/java/Modelo/Cliente.java
@@ -1,14 +1,23 @@
 package Modelo;
 
+import androidx.annotation.NonNull;
+import androidx.room.PrimaryKey;
+
 public class Cliente {
+    @PrimaryKey
+    @NonNull
     String clientNo;
+    @NonNull
     String fName;
+    @NonNull
     String lName;
     String telNo;
+    @NonNull
     String prefType;
+    @NonNull
     String maxRent;
 
-    public Cliente(String clientNo, String fName, String lName, String telNo, String prefType, String maxRent) {
+    public Cliente(@NonNull String clientNo, @NonNull String fName, @NonNull String lName, String telNo, @NonNull String prefType, @NonNull String maxRent) {
         this.clientNo = clientNo;
         this.fName = fName;
         this.lName = lName;
@@ -17,27 +26,30 @@ public class Cliente {
         this.maxRent = maxRent;
     }
 
+    @NonNull
     public String getClientNo() {
         return clientNo;
     }
 
-    public void setClientNo(String clientNo) {
+    public void setClientNo(@NonNull String clientNo) {
         this.clientNo = clientNo;
     }
 
+    @NonNull
     public String getfName() {
         return fName;
     }
 
-    public void setfName(String fName) {
+    public void setfName(@NonNull String fName) {
         this.fName = fName;
     }
 
+    @NonNull
     public String getlName() {
         return lName;
     }
 
-    public void setlName(String lName) {
+    public void setlName(@NonNull String lName) {
         this.lName = lName;
     }
 
@@ -49,19 +61,33 @@ public class Cliente {
         this.telNo = telNo;
     }
 
+    @NonNull
     public String getPrefType() {
         return prefType;
     }
 
-    public void setPrefType(String prefType) {
+    public void setPrefType(@NonNull String prefType) {
         this.prefType = prefType;
     }
 
+    @NonNull
     public String getMaxRent() {
         return maxRent;
     }
 
-    public void setMaxRent(String maxRent) {
+    public void setMaxRent(@NonNull String maxRent) {
         this.maxRent = maxRent;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "clientNo='" + clientNo + '\'' +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", telNo='" + telNo + '\'' +
+                ", prefType='" + prefType + '\'' +
+                ", maxRent='" + maxRent + '\'' +
+                '}';
     }
 }
