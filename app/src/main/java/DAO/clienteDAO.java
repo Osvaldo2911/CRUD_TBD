66e1dba51bd23ccda1d6f3cfeb538786b28fd52d
Osvaldo <osvaldoadelatorretec@gmail.com>
2021-12-13 13:38:51 -0600

    Se agrego las consultas a cliente dao

--- app/src/main/java/DAO/clienteDAO.java
+++ app/src/main/java/DAO/clienteDAO.java
@@ -4,21 +4,28 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
+import Modelo.ClienteT;
 import Modelo.UsuarioT;
 
 @Dao
 public interface clienteDAO {
 
     @Query("SELECT * FROM ClienteT WHERE nombre=:nombreEntrada AND userName=:nomUser AND contraseña=:con")
-    public UsuarioT buscarUsuario(String nombreEntrada, String nomUser, String con);
+    public ClienteT buscarUsuario(String nombreEntrada, String nomUser, String con);
 
     //Altas
     @Insert
-    public void insertarUsuario(UsuarioT alum);
+    public void insertarUsuario(ClienteT alum);
 
-    @Query("delete from UsuarioT where userName=:nomUser")
-    public UsuarioT eliminarUsuario(String nomUser);
+    @Query("delete from ClienteT where userName=:nomUser")
+    public ClienteT eliminarUsuario(String nomUser);
 
-    @Query("update UsuarioT set nombre=:nombreN,userName=:nomUser,contraseña=:con where userName=:nomUser")
-    public UsuarioT modificarUsuario(String nombreN, String nomUser, String con);
+    @Query("update ClienteT set nombre=:nombreN,userName=:nomUser,contraseña=:con where userName=:nomUser")
+    public ClienteT modificarUsuario(String nombreN, String nomUser, String con);
+
+    @Query("SELECT * FROM ClienteT")
+    public List<ClienteT> obtenerTodos();
+
+    @Query("Select * from ClienteT where ClientNo LIKE '%\" + id + \"%' AND fName LIKE '%\" + pa + \"%' AND IName LIKE '%\" + sa + \"%' AND telNo LIKE '%\" + te + \"%' AND prefType LIKE '%\" + ti + \"%' AND maxRent LIKE '%\" + re + \"%';")
+    public List<ClienteT> obtenerTodosPersonalizado();
 }
