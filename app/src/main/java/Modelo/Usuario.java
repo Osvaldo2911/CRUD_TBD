e7cd7d5fe8a11a232d2bfd710fc3e7bfa40f8bd9
Osvaldo <osvaldoadelatorretec@gmail.com>
2021-12-13 10:14:02 -0600

    Se modifico la clase de Usuario, se le agrego lo nesesario para ser una entidad de room

--- app/src/main/java/Modelo/Usuario.java
+++ app/src/main/java/Modelo/Usuario.java
@@ -1,38 +1,56 @@
 package Modelo;
 
+import androidx.annotation.NonNull;
+import androidx.room.PrimaryKey;
+
 public class Usuario {
+    @NonNull
     String nombre;
+    @PrimaryKey
+    @NonNull
     String userName;
+    @NonNull
     String contraseña;
 
-    public Usuario(String nombre, String userName, String contraseña) {
+    public Usuario(@NonNull String nombre, @NonNull String userName, @NonNull String contraseña) {
         this.nombre = nombre;
         this.userName = userName;
         this.contraseña = contraseña;
     }
 
+    @NonNull
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setNombre(@NonNull String nombre) {
         this.nombre = nombre;
     }
 
+    @NonNull
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
+    public void setUserName(@NonNull String userName) {
         this.userName = userName;
     }
 
+    @NonNull
     public String getContraseña() {
         return contraseña;
     }
 
-    public void setContraseña(String contraseña) {
+    public void setContraseña(@NonNull String contraseña) {
         this.contraseña = contraseña;
     }
 
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", userName='" + userName + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                '}';
+    }
 }
