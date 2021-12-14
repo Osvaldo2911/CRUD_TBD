0dfc128f05395e90aa3b59acf4f0688917a2a67a
Osvaldo <osvaldoadelatorretec@gmail.com>
2021-12-13 21:19:57 -0600

    Se agrego los dao de cliente

--- app/src/main/java/DAO/clienteDAO.java
+++ app/src/main/java/DAO/clienteDAO.java
@@ -1,31 +1,31 @@
 package DAO;
 
+import androidx.annotation.NonNull;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
 import Modelo.ClienteT;
-import Modelo.UsuarioT;
 
 @Dao
 public interface clienteDAO {
 
-    @Query("SELECT * FROM ClienteT WHERE nombre=:nombreEntrada AND userName=:nomUser AND contraseña=:con")
-    public ClienteT buscarUsuario(String nombreEntrada, String nomUser, String con);
+    @Query("SELECT * FROM ClienteT WHERE clientNo=:clientNo")
+    public ClienteT buscarUsuario(String clientNo);
 
     //Altas
     @Insert
     public void insertarUsuario(ClienteT alum);
 
-    @Query("delete from ClienteT where userName=:nomUser")
-    public ClienteT eliminarUsuario(String nomUser);
+    @Query("delete from ClienteT where ClientNo=:clientNo")
+    public ClienteT eliminarUsuario(String clientNo);
 
-    @Query("update ClienteT set nombre=:nombreN,userName=:nomUser,contraseña=:con where userName=:nomUser")
-    public ClienteT modificarUsuario(String nombreN, String nomUser, String con);
+    @Query("update ClienteT set fName=:fName,lName=:lName,telNo=:telNo,prefType=:prefType,maxRent=:maxRent where clientNo=:clientNo")
+    public ClienteT modificarUsuario(String clientNo, String fName, String lName, String telNo, String prefType, String maxRent);
 
     @Query("SELECT * FROM ClienteT")
     public List<ClienteT> obtenerTodos();
 
-    @Query("Select * from ClienteT where ClientNo LIKE '%\" + id + \"%' AND fName LIKE '%\" + pa + \"%' AND IName LIKE '%\" + sa + \"%' AND telNo LIKE '%\" + te + \"%' AND prefType LIKE '%\" + ti + \"%' AND maxRent LIKE '%\" + re + \"%';")
-    public List<ClienteT> obtenerTodosPersonalizado();
+    @Query("Select * from ClienteT where ClientNo LIKE '%\"' + ClientNo=:clientNo + '\"%' AND fName LIKE '%\"' + fName=:fName + '\"%' AND lName LIKE '%\"' + lName=:lName+' \"%' AND telNo LIKE '%\" '+ telNo=:telNo + '\"%' AND prefType LIKE '%\" '+ prefType=:prefType +' \"%' AND maxRent LIKE '%\"' + maxRent=:maxRent +' \"%';")
+    public List<ClienteT> obtenerTodosPersonalizado(String clientNo, String fName, String lName, String telNo, String prefType, String maxRent);
 }
