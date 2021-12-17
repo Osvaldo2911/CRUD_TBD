20bd29c70ca02541180282f60e596e9fcf668ee4
Osvaldo <osvaldoadelatorretec@gmail.com>
2021-12-17 00:20:40 -0600

    Se agrego validacion al Registro usuario

--- app/src/main/java/com/example/crud_tbd/Activity_Registro.java
+++ app/src/main/java/com/example/crud_tbd/Activity_Registro.java
@@ -1,13 +1,10 @@
 package com.example.crud_tbd;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.*;
 import android.widget.Button;
@@ -15,8 +12,6 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.List;
 
 import ConexionBD.Conexion;
 import Modelo.UsuarioT;
@@ -44,8 +39,6 @@ public class Activity_Registro extends AppCompatActivity implements View.OnTouch
         contra = findViewById(R.id.et_ps_contraRegistrar);
         confirmar = findViewById(R.id.et_ps_contraConfirmarRegistrar);
         terminos = findViewById(R.id.terminosCondiciones);
-
-        resultados = findViewById(R.id.lbl_resultados);
 
         user.addTextChangedListener(new TextWatcher() {
             @Override
@@ -98,32 +91,8 @@ public class Activity_Registro extends AppCompatActivity implements View.OnTouch
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (contra.getText().length() >= 8) {
-                    int n = 0;
-                    int ma = 0;
-                    int mi = 0;
-                    int sim = 0;
-                    for (int i = 0; i < contra.getText().length(); i++) {
-                        String cont = contra.getText().toString();
-                        int a = cont.charAt(i);
-                        if (a >= 48 && a <= 57) {
-                            n = n + 1;
-                        }
-                        if (a >= 65 && a <= 90) {
-                            ma = ma + 1;
-                        }
-                        if (a >= 97 && a <= 122) {
-                            mi = mi + 1;
-                        }
-                        if (a >= 33 && a <= 47 || a >= 91 && a <= 96 || a >= 123 && a <= 126) {
-                            sim = sim + 1;
-                        }
-                    }
 
-                    if (n>0&&ma>0&&mi>0&&sim>0){
-                        cn=contra.getText().toString();
-                    }
-                }
+
             }
         });
 
@@ -137,42 +106,68 @@ public class Activity_Registro extends AppCompatActivity implements View.OnTouch
 
                 /// inicio hilo
 
-
         conexion = Conexion.gettAppDatabase(getBaseContext());
 
-        nom = nombre.getText().toString();
-        //email = user.getText().toString();
-        cn = contra.getText().toString().trim();
-        cnc = confirmar.getText().toString().trim();
-
-        resultados.setText("nom: "+nom);
+                nom = nombre.getText().toString();
+                email = user.getText().toString();
+                cn = contra.getText().toString().trim();
+                cnc = confirmar.getText().toString();
 
         if (!nom.equals("")){
-            if (!email.equals("")){
+
+            if (!email.equals("")) {
 
                 if (!cn.equals("")){
-                    if (!(cnc.equals(""))  && cn.equals(cnc)){
-                        if (terminos.isChecked()){
-                            //--------------------------------------------------
-                            resultados.setText("Cumples todos los requisitos");
-                            UsuarioT busqueda = conexion.usuarioDAO().usuarioDisponible(email);
 
-                            if (busqueda == null){
-                                new Thread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(getBaseContext(), "Registro con exito", Toast.LENGTH_LONG).show();
-                                    }
-                                }).start();
-                                conexion.usuarioDAO().insertarUsuario(new UsuarioT(nom,email,cn));
-                                nombre.setText("");
-                                user.setText("");
-                                contra.setText("");
-                                confirmar.setText("");
-                                nom ="";
-                                cn="";
-                                cnc="";
-                                terminos.setChecked(false);
+                    if (!(cnc.equals(""))){
+
+                        if (terminos.isChecked()){
+                            UsuarioT busqueda;
+                            boolean aceptar = false;
+                            String ce = "";
+                            String con ="";
+                            ce = emailValidar(user);
+                            con = contraseñaValidar(contra);
+                            if(!(ce.equals("") && !(con.equals("")))){
+                                if (con.equals(cnc)){
+                                    aceptar=true;
+                                }else{
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(getBaseContext(), "Confirmar contraseña tiene errores", Toast.LENGTH_LONG).show();
+                                        }
+                                    }).start();
+                                }
+                            }
+                            if (aceptar==true){
+                                busqueda = conexion.usuarioDAO().usuarioDisponible(email);
+                                if (busqueda == null) {
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(getBaseContext(), "Registro con exito", Toast.LENGTH_LONG).show();
+                                        }
+                                    }).start();
+
+                                    conexion.usuarioDAO().insertarUsuario(new UsuarioT(nom,email,cn));
+                                    nombre.setText("");
+                                    user.setText("");
+                                    contra.setText("");
+                                    confirmar.setText("");
+                                    nom ="";
+                                    cn="";
+                                    cnc="";
+                                    terminos.setChecked(false);
+
+                                }else{
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(getBaseContext(), "Correo en uso", Toast.LENGTH_LONG).show();
+                                        }
+                                    }).start();
+                                }
                             }
 
                             //--------------------------------------------------
@@ -188,7 +183,7 @@ public class Activity_Registro extends AppCompatActivity implements View.OnTouch
                         new Thread(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(getBaseContext(), "Rellena los campos", Toast.LENGTH_LONG).show();
+                                Toast.makeText(getBaseContext(), "Acepte los terminos y condiciones", Toast.LENGTH_LONG).show();
                             }
                         }).start();
                     }
@@ -234,8 +229,55 @@ public class Activity_Registro extends AppCompatActivity implements View.OnTouch
         return false;
     }
 
+    public String contraseñaValidar(EditText c) {
 
+        if (c.getText().length() >= 8) {
+            int n = 0;
+            int ma = 0;
+            int mi = 0;
+            int sim = 0;
+            for (int i = 0; i < c.getText().length(); i++) {
+                String cont = c.getText().toString();
+                int a = cont.charAt(i);
+                if (a >= 48 && a <= 57) {
+                    n = n + 1;
+                }
+                if (a >= 65 && a <= 90) {
+                    ma = ma + 1;
+                }
+                if (a >= 97 && a <= 122) {
+                    mi = mi + 1;
+                }
+                if (a >= 33 && a <= 47 || a >= 91 && a <= 96 || a >= 123 && a <= 126) {
+                    sim = sim + 1;
+                }
+            }
 
+            if (n > 0 && ma > 0 && mi > 0 && sim > 0) {
+                return c.getText().toString();
+            }
+        }
+        return "error";
+    }
 
+    public String emailValidar(EditText es){
+        String e = es.getText().toString();
+        String res;
+        int cont = 0, cont2 = 0;
+        for (int i = 0; i < e.length(); i++) {
+            char s = e.charAt(i);
+            if (s == '@') {
+                cont = cont + 1;
+            } else if (s == '.') {
+                cont2 = cont2 + 1;
+            } else {
+
+            }
+            if (cont >= 1 && cont2 >= 1) {
+                return e;
+            }
+        }
+        return "error";
+    }
 
 }
